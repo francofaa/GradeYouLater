7a5615bdf84f02394c24ebb456421d9f202f8580
FrancoFaa <franco.faa@gmail.com>
2016-12-15 15:38:05 -0500

    an older update
    this will be superseded

--- functions.js
+++ functions.js
@@ -1,3 +1,23 @@
+/*
+
+  if (dec >= 90) {
+      var letter = A;
+  } else if (dec < 90 && dec >= 80) {
+      var letter = B;
+  }
+    else if (dec < 80 && dec >= 70) {
+      var letter = C;
+  }
+    else if (dec < 70 && dec >= 60) {
+      var letter = D;
+  }
+    else if (dec < 60) {
+      var letter = F;
+  } else {
+      var letter = "Please enter total points";
+  }
+
+*/
 function addOne() {
   document.getElementById("tally").value++;
 }
@@ -10,25 +30,8 @@ function genGrade() {
   var num = document.getElementById("tally").value;
   var den = document.getElementById("totalPoints").value;
   var dec = +(((num / den) * 100).toFixed(2)); 
- 
-  if (dec >= 90.00) {
-      var letter = A;
-  } else if (dec < 90.00 && dec >= 80.00) {
-      var letter = B;
-  }
-    else if (dec < 80 && dec >= 70) {
-      var letter = C;
-  }
-    else if (dec < 70 && dec >= 60) {
-      var letter = D;
-  }
-    else if (dec < 60.00) {
-      var letter = F;
-  } else {
-      var letter = "Please enter total points";
-  }
+  document.getElementById("grade").innerHTML = "Grade: " + dec + "%"; 
 
-  document.getElementById("grade").innerHTML = "Grade: " + letter + dec + "%"; 
   
 }
 
