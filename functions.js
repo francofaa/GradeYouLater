683f11a1cc2fde9d19e08097547306874d8e4f9e
francofaa <franco.faa@gmail.com>
2016-12-15 15:38:26 -0500

    newer update
    a more new update

--- functions.js
+++ functions.js
@@ -1,3 +1,9 @@
+var num, den, dec;
+
+num = document.getElementById("tally").value;
+den = document.getElementById("totalPoints").value;
+dec = +(((num / den) * 100).toFixed(2)); 
+
 function addOne() {
   document.getElementById("tally").value++;
 }
@@ -7,10 +13,23 @@ function loseOne() {
 }
 
 function genGrade() { 
-  var num = document.getElementById("tally").value;
-  var den = document.getElementById("totalPoints").value;
-  var dec = +(((num / den) * 100).toFixed(2)); 
- 
+  
+  document.getElementById("grade").innerHTML = "Grade: " + dec + "%";
+}
+
+function reset() {
+   document.getElementById("tally").value = 0;
+}
+
+gradebookArray = [];
+
+function enterGrade() {
+  
+  
+  document.getElementById("gradebook").innerHTML = gradebookArray;
+}
+
+ /*
   if (dec >= 90.00) {
       var letter = A;
   } else if (dec < 90.00 && dec >= 80.00) {
@@ -27,11 +46,4 @@ function genGrade() {
   } else {
       var letter = "Please enter total points";
   }
-
-  document.getElementById("grade").innerHTML = "Grade: " + letter + dec + "%"; 
-  
-}
-
-function reset() {
-   document.getElementById("tally").value = 0;
-}
+*/
