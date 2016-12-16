302b7222d828c45137fcac75b45707013960eb9d
francofaa <franco.faa@gmail.com>
2016-12-15 19:08:27 -0500

    major update
    It has become mildly functional!

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # GradeYouLater
-A simple grading calculator
+I wanted to create a simple grading system, hopefully to inevitably turn it into an app.
+
+## Goals
+The user will be able to
+
+- Set an assignment
+- Ascribe a point value to that assignment
+- Enter in a student's name
+- Increment the student's score by 1 as the user goes through grading the assignment
+- Decrement in case of error
+- Render a percentage grade based on the score
+- Push the score, assignment, and associate student to a list
+- Export that list to CSV or another format for entering into a gradebook
+
+## Origin
+
+I was helping my girlfriend grade her students' worksheets for her French class. We went section by section, tallied up the points, and took the points and got the percentage right. She then entered that grade into a gradebook. 
+
+I wanted to make a simple app that could tally the student's score one by one and figure out the percentage from the total, and after that I realized you could automate so much more. My next step is getting the thing to email you with a CSV spreadsheet file so that you could enter the grades in using whatever gradebook app you use.
--- functions.js
+++ functions.js
@@ -1,9 +1,4 @@
-<<<<<<< HEAD
 var num, den, dec;
-
-num = document.getElementById("tally").value;
-den = document.getElementById("totalPoints").value;
-dec = +(((num / den) * 100).toFixed(2)); 
 
 function addOne() {
   document.getElementById("tally").value++;
@@ -14,9 +9,18 @@ function loseOne() {
 }
 
 function genGrade() { 
+  num = document.getElementById("tally").value;
+  den = document.getElementById("totalPoints").value;
+  dec = +(((num / den) * 100).toFixed(2));
+  if (isNaN(dec)) {
+    dec = "<i style='color: red;'>!!</i>"
+    document.getElementById("grade").innerHTML = "Please enter a number";
+  } else {
+    document.getElementById("grade").innerHTML = "Grade: " + dec + "%";
+  }
   
-  document.getElementById("grade").innerHTML = "Grade: " + dec + "%";
 }
+
 
 function reset() {
    document.getElementById("tally").value = 0;
@@ -25,56 +29,40 @@ function reset() {
 gradebookArray = [];
 
 function enterGrade() {
-  
-  
-  document.getElementById("gradebook").innerHTML = gradebookArray;
-}
-
- /*
-  if (dec >= 90.00) {
-=======
-/*
-
   if (dec >= 90) {
->>>>>>> origin/master
-      var letter = A;
+      var letter = "A";
   } else if (dec < 90 && dec >= 80) {
-      var letter = B;
+      var letter = "B";
   }
     else if (dec < 80 && dec >= 70) {
-      var letter = C;
+      var letter = "C";
   }
     else if (dec < 70 && dec >= 60) {
-      var letter = D;
+      var letter = "D";
   }
     else if (dec < 60) {
-      var letter = F;
+      var letter = "F";
   } else {
       var letter = "Please enter total points";
   }
-<<<<<<< HEAD
-*/
-=======
-
-*/
-function addOne() {
-  document.getElementById("tally").value++;
+  gradebookArray.push({
+    "student": document.getElementById('studentName').value,
+    "assignment": document.getElementById('assignment').value,
+    "score": dec,
+    "letter": letter
+  });
+    var table = document.getElementById('gradebook');
+    var row = table.insertRow(1);
+    var cell1 = row.insertCell(0);
+    var cell2 = row.insertCell(1);
+    var cell3 = row.insertCell(2);
+    var cell4 = row.insertCell(3);
+    cell1.innerHTML = gradebookArray[gradebookArray.length - 1].student;
+    cell2.innerHTML = gradebookArray[gradebookArray.length - 1].assignment;
+    cell3.innerHTML = gradebookArray[gradebookArray.length - 1].score;
+    cell4.innerHTML = gradebookArray[gradebookArray.length - 1].letter;
 }
 
-function loseOne() {
-  document.getElementById("tally").value--;
-}
+function emailBook() {
 
-function genGrade() { 
-  var num = document.getElementById("tally").value;
-  var den = document.getElementById("totalPoints").value;
-  var dec = +(((num / den) * 100).toFixed(2)); 
-  document.getElementById("grade").innerHTML = "Grade: " + dec + "%"; 
-
-  
 }
-
-function reset() {
-   document.getElementById("tally").value = 0;
-}
->>>>>>> origin/master
